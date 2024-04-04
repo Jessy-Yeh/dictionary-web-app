283b20fb79182799a04040a5765a19012ddc2234
Jessy Yeh <jiejun.yeh@gmail.com>
2024-04-04 11:28:46 +0100

    add logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import { Select } from "@mantine/core";
 import { Switch } from "@mantine/core";
+import { IconBook } from "@tabler/icons-react";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={styles.menu}>
-      <Select
-        variant="unstyled"
-        placeholder="Pick value"
-        data={["Serif", "Angular", "Vue", "Svelte"]}
-        defaultValue="Serif"
-      />
-      <div className={styles.line}></div>
-      <Switch color="grape" />
+      <IconBook />
+      <div className={styles.menuRight}>
+        <Select
+          variant="unstyled"
+          placeholder="Pick value"
+          data={["Serif", "Angular", "Vue", "Svelte"]}
+          defaultValue="Serif"
+        />
+        <div className={styles.line}></div>
+        <Switch color="grape" />
+      </div>
     </nav>
   );
 };
